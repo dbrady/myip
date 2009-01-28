ec262e376826e23d7aeb04867c1838fcc25caad1
David Brady <dbrady@shinybit.com>
2009-01-28 10:31:34 -0700

    Put IP address in title bar

--- myip.rb
+++ myip.rb
@@ -2,5 +2,6 @@ require 'rubygems'
 require 'sinatra'
 
 get '*' do
-  "<h1>Your IP Address is #{request.env['REMOTE_ADDR']}</h1>"
+  ip = request.env['REMOTE_ADDR']
+  "<html><head><title>#{ip}</title></head><body><h1>Your IP Address is #{ip}</h1></body></html>"
 end 
